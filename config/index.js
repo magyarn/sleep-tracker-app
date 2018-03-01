a0641b1055a38bf2b06e1cdce39155680048ad83
Nathan Magyar <magyarn@umich.edu>
2018-03-01 09:00:46 -0500

    Fix asset path again

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/sleep-tracker-app/',
+    assetsPublicPath: '/my-sleep-score/',
 
     /**
      * Source Maps
