65964b92bf123fb66525579f117597da0a1aabac
Nathan Magyar <magyarn@umich.edu>
2018-03-13 14:54:23 -0400

    adjust navigation paths

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,13 +2,13 @@
   <header>
     <b-container>
       <b-navbar toggleable class="sleep-nav px-0" type="dark">
-        <b-navbar-brand to="/">#SleepChallenge</b-navbar-brand>
+        <b-navbar-brand to="/my-sleep-score/">#SleepChallenge</b-navbar-brand>
         <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
         <b-collapse is-nav id="nav_dropdown_collapse">
           <b-navbar-nav role="navigation" class="ml-auto">
-            <b-nav-item to="/" exact active-class="sleep-active-nav-item">Home</b-nav-item>
-            <b-nav-item to="/sleep-strategies" exact active-class="sleep-active-nav-item">Sleep Strategies</b-nav-item>
-            <b-nav-item to="/sleep-journal" exact active-class="sleep-active-nav-item">My Sleep Journal</b-nav-item>
+            <b-nav-item to="/my-sleep-score/" exact active-class="sleep-active-nav-item">Home</b-nav-item>
+            <b-nav-item to="/my-sleep-score/sleep-strategies/" exact active-class="sleep-active-nav-item">Sleep Strategies</b-nav-item>
+            <b-nav-item to="/my-sleep-score/sleep-journal/" exact active-class="sleep-active-nav-item">My Sleep Journal</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
