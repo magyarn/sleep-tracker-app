6bbab312c4c0fd3c0560563fca1dcd9a52b015ae
Nathan Magyar <magyarn@umich.edu>
2019-09-23 09:25:43 -0400

    Remove text

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -7,7 +7,8 @@
             v-if="$mq.resize && $mq.below('767px')"
             src="../../static/img/moon-and-sheep.png"
             alt="sheep sleeping on the moon"
-            class="moon-graphic">
+            class="moon-graphic"
+          />
           <h1 class="hero-heading">Go for 7</h1>
           <p class="hero-tagline">SEVEN HOURS OF SLEEP A NIGHT</p>
         </b-col>
@@ -17,9 +18,9 @@
           <h2 class="mt-4 mb-2">Sleep Matters.</h2>
           <p class="pt-2">
             Your health, mood, relationships, and work performance
-          can all improve with better sleep. Poor sleep can lead to depression
-          and weight gain, while good sleep can help you feel better, think
-          clearer, and manage stress more easily.
+            can all improve with better sleep. Poor sleep can lead to depression
+            and weight gain, while good sleep can help you feel better, think
+            clearer, and manage stress more easily.
           </p>
           <p>
             Take action to learn about your sleep habits and how you may be able to improve your quality
@@ -28,7 +29,7 @@
           <b-button to="/sleep-journal" size="lg" class="btn-success mt-4">Track My Sleep</b-button>
         </b-col>
         <b-col v-if="$mq.resize && $mq.above('768px')" xl="6" lg="6" md="6" class="moon-graphic">
-          <img src="../../static/img/moon-and-sheep.png" alt="sheep sleeping on the moon">
+          <img src="../../static/img/moon-and-sheep.png" alt="sheep sleeping on the moon" />
         </b-col>
       </b-row>
     </b-container>
@@ -40,25 +41,24 @@
             <iframe
               class="youtube-video"
               src="https://www.youtube.com/embed/4j2cvAZYaUg"
-              frameborder="0" allow="autoplay; encrypted-media"
-              allowfullscreen>
-            </iframe>
+              frameborder="0"
+              allow="autoplay; encrypted-media"
+              allowfullscreen
+            ></iframe>
           </b-col>
           <b-col class="about-text" lg="6" xl="6">
             <p>
               The Sleep Challenge was developed by the University of Michigan’s
               Sleep Disorders Center as a part of the Teach-Out entitled
-              <a href="https://www.coursera.org/learn/sleepdeprivation">
-                “Sleep Deprivation: Habits, Solutions, and Strategies”.
-              </a>
-            </p>
-            <p>
-              The Teach-Out will be open from <strong>Monday March 12th through Sunday,
-                March 25th</strong>.
+              <a
+                href="https://www.coursera.org/learn/sleepdeprivation"
+              >“Sleep Deprivation: Habits, Solutions, and Strategies”.</a>
             </p>
             <p>
               To learn more about this and other Teach-Outs, visit
-              <a href="http://ai.umich.edu/teach-out/">teachout.org</a>.
+              <a
+                href="http://ai.umich.edu/teach-out/"
+              >teachout.org</a>.
             </p>
           </b-col>
         </b-row>
@@ -69,21 +69,21 @@
 
 <script>
 export default {
-  name: 'LandingPage'
-}
+  name: "LandingPage"
+};
 </script>
 
 <style lang="scss">
-@import '../main.scss';
+@import "../main.scss";
 .intro-wrapper {
-  background-image: url('../../static/img/background.png');
+  background-image: url("../../static/img/background.png");
   background-repeat: repeat-y;
   background-position: center;
   background-attachment: fixed;
   background-size: contain;
   color: $white;
   @media (max-width: 576px) {
-    background-image: url('../../static/img/mobile-background.png')
+    background-image: url("../../static/img/mobile-background.png");
   }
 }
 
@@ -116,7 +116,7 @@ export default {
 }
 
 .about-section {
-  background-image: url('../../static/img/clouds-bg.png');
+  background-image: url("../../static/img/clouds-bg.png");
   background-repeat: no-repeat;
   background-position: top;
   background-size: cover;
@@ -127,7 +127,7 @@ export default {
   margin-top: 3rem;
   .youtube-video {
     width: 100%;
-    height: 315px;;
+    height: 315px;
   }
   .about-heading {
     margin-bottom: 3rem;
@@ -152,7 +152,7 @@ export default {
   justify-content: center;
   align-items: center;
   li {
-    padding: 1rem .5rem;
+    padding: 1rem 0.5rem;
   }
   .share-btn {
     height: 55px;
@@ -164,7 +164,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    transition: all .2s ease;
+    transition: all 0.2s ease;
     &:hover {
       background-color: $elephantGrey;
       border: 1px solid rgba(255, 255, 255, 0);
